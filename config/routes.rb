3fc66a80859e1241a165f30f612114f3a14fa2a4
rafaDev <rrg1459@hotmail.com>
2025-08-13 21:42:11 -0400

    add schwab to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     get '/etrade/callback', to: 'etrade#callback'
     get '/schwab/start_auth', to: 'schwab#start_auth'
     get '/schwab/callback', to: 'schwab#callback'
+    get '/schwab/refresh_access_token', to: 'schwab#refresh_access_token'
+    get '/schwab/get_account_numbers', to: 'schwab#get_account_numbers'
+    get '/schwab/get_account_trades', to: 'schwab#get_account_trades'
   end
     root "home#initial"
 
