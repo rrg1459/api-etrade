55e9f7512f1e00a31c3d2e34cffc24b302429707
rafaDev <rrg1459@hotmail.com>
2025-08-14 11:23:27 -0400

    add valid methods in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# app/models/user.rb
+
 class User < ApplicationRecord
+  # ... otras validaciones y relaciones
+
+  def schwab_token_is_valid?
+    schwab_access_token.present? && schwab_access_token_expires_at.present? && schwab_access_token_expires_at > Time.now.utc
+  end
+
+  def schwab_refresh_token_is_valid?
+    schwab_refresh_token.present? && schwab_refresh_token_expires_at.present? && schwab_refresh_token_expires_at > Time.now.utc
+  end
 end
