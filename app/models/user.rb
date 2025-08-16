c408a5f9b60c970a88aa4ca76f1670f52417c18a
rafaDev <rrg1459@hotmail.com>
2025-08-15 21:16:24 -0400

    update model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@
 class User < ApplicationRecord
   # ... otras validaciones y relaciones
 
-  def schwab_token_is_valid?
-    schwab_access_token.present? && schwab_access_token_expires_at.present? && schwab_access_token_expires_at > Time.now.utc
-  end
+  # def schwab_token_is_valid?
+  #   schwab_access_token.present? && schwab_access_token_expires_at.present? && schwab_access_token_expires_at > Time.now.utc
+  # end
 
-  def schwab_refresh_token_is_valid?
-    schwab_refresh_token.present? && schwab_refresh_token_expires_at.present? && schwab_refresh_token_expires_at > Time.now.utc
-  end
+  # def schwab_refresh_token_is_valid?
+  #   schwab_refresh_token.present? && schwab_refresh_token_expires_at.present? && schwab_refresh_token_expires_at > Time.now.utc
+  # end
+
+  has_many :brokers
+  has_many :broker_conexion_variables
+
+  has_many :transactions, through: :brokers
 end
